0c2d61455597b36aae82e323cfe6e192e07d589a
Michael Dick <michael@m1k3.net>
2016-04-28 00:32:44 -0700

    Fixing color algo bug

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -27,7 +27,7 @@ class Items extends React.Component {
     return (
       <ul className="Items">
         { this.keys.map((key, index) => {
-          i = (Object.keys(colors).length % index == 0 && index > 0) ? 0 : i+1;
+          i = (Object.keys(colors).length % index == 0 && index <= 0) ? 0 : i+1;
           return <Item k={key} item={items[key]} index={index} color={colors[i]} />;
         })}
       </ul>
